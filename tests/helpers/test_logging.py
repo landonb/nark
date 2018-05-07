07fba768890e35335e15eb669729a1d30876cfa2
HotOffThe Hamster <hotoffthehamster@gmail.com>
2018-05-06 23:33:44 -0500

    DRY: Move logging setup functions from CLI to new helpers module.

---
+++ tests/helpers/test_logging.py
@@ -0,0 +1,23 @@
+# -*- encoding: utf-8 -*-
+
+from __future__ import absolute_import, unicode_literals
+
+import logging
+
+import pytest
+from hamster_lib.helpers import logging as logging_helpers
+
+class TestSetupHandler(object):
+    def test_get_formatter_basic(self, mocker):
+        """Test formatter fetcher."""
+        formatter = logging_helpers.formatter_basic()
+        assert 'levelname' in formatter
+
+    def test_setup_handler_stream_handler(self, mocker):
+        """Test logging setup."""
+        stream_handler = logging.StreamHandler()
+        formatter = logging_helpers.formatter_basic()
+        logger = mocker.MagicMock()
+        logging_helpers.setupHandler(stream_handler, formatter, logger)
+        logger.addHandler.assert_called_with(stream_handler)
+
