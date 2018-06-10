87e9d67de921fa441a3e944f1e67a3d116a1c344
HotOffThe Hamster <hotoffthehamster@gmail.com>
2018-06-09 19:09:29 -0500

    Helper: Strings methods.
    - comma_or_join: Join array parts with commas (","),
      except separate last two elementes with "or\'.
    - format_value_truncate: Truncate string at specified
      width, appending ellipses to convey truncationness.

---
+++ hamster_lib/helpers/strings.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+
+# This file is part of 'hamster-lib'.
+#
+# 'hamster-lib' is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# 'hamster-lib' is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with 'hamster-lib'.  If not, see <http://www.gnu.org/licenses/>.
+
+"""This module provides Hamster raw fact parsing-related functions."""
+
+from __future__ import absolute_import, unicode_literals
+
+import click
+from six import text_type
+
+
+__all__ = [
+    'comma_or_join',
+]
+
+
+def comma_or_join(seq):
+    # Need os.linesep or is \n fine?
+    seq = [part.replace('\n', '\\n') for part in seq]
+    if len(seq) > 1:
+        sep_str = '{} or {}'.format(', '.join(seq[:-1]), seq[-1])
+    else:
+        sep_str = seq[0]
+    return sep_str
+
+
+def format_value_truncate(val, trunc_width=None):
+    if not val:
+        return val
+    # (lb): First attempt was to only short first line:
+    #   vals = val.splitlines()
+    #   if len(vals) > 1:
+    #       val = vals[0] + '...'
+    # but replacing newlines with representation of same
+    # is more meaningful, and less confusing to end user.
+    val = '\\n'.join(text_type(val).splitlines())
+    if trunc_width is None:
+        # (lb): What's a good value? A third of the terminal?
+        term_width = click.get_terminal_size()[0]
+        trunc_width = int(term_width * (1 / 3.))
+    if len(val) > trunc_width and trunc_width >= 0:
+        val = val[:trunc_width - 3] + '...'
+    return val
+
