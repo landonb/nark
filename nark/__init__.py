2fb90e1df61f949d0f21fd063ca8afc5970babc4
HotOffThe Hamster <hotoffthehamster@gmail.com>
2018-06-13 00:28:17 -0500

    Rebrand: Add nark'ed PyPI meta vals to top-level nark/__init__.py.

--- nark/__init__.py
+++ nark/__init__.py
@@ -1,26 +1,54 @@
 # -*- coding: utf-8 -*-
 
-# This file is part of 'hamster_lib'.
+# This file is part of 'nark'.
 #
-# 'hamster_lib' is free software: you can redistribute it and/or modify
+# 'nark' is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# 'hamster_lib' is distributed in the hope that it will be useful,
+# 'nark' is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with 'hamster_lib'.  If not, see <http://www.gnu.org/licenses/>.
+# along with 'nark'.  If not, see <http://www.gnu.org/licenses/>.
 
 """hamster-lib provides generic time tracking functionality."""
 
-from .control import REGISTERED_BACKENDS, HamsterControl  # NOQA
-from .items.activity import Activity
-from .items.category import Category
-from .items.fact import Fact
-from .items.tag import Tag
+from __future__ import absolute_import, unicode_literals
 
-__version__ = '0.13.2'
+from gettext import gettext as _
+
+# Export classes from here for other application to more easily import.
+from .control import REGISTERED_BACKENDS, HamsterControl  # noqa: F401
+from .items.activity import Activity  # noqa: F401
+from .items.category import Category  # noqa: F401
+from .items.fact import Fact  # noqa: F401
+from .items.tag import Tag  # noqa:
+
+
+# SYNC_UP: nark/__init__.py <=> dob/__init__.py
+__author__ = 'HotOffThe Hamster'
+__author_email__ = 'hotoffthehamster+nark@gmail.com'
+__version__ = '3.0.0.a1'
+__appname__ = 'nark'
+__pipname__ = __appname__
+__briefly__ = _(
+    'Robot backend for personal journaling and professional time tracking software (like `dob`).'
+)
+__projurl__ = 'https://github.com/hotoffthehamster/nark'
+__keywords__ = ' '.join([
+    'journal',
+    'diary',
+    'timesheet',
+    'timetrack',
+    'jrnl',
+    'rednotebook',
+    'todo.txt',
+    'prjct',
+    'hamster',
+    'fact',
+])
+
