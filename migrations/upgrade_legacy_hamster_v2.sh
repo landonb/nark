3028220264e065a356ce3e765cc5167eef76e25f
HotOffThe Hamster <hotoffthehamster@gmail.com>
2018-06-12 22:59:05 -0500

    Legal: Update copyright headers: 'hamster_lib' → 'nark'.

--- migrations/upgrade_legacy_hamster_v2.sh
+++ migrations/upgrade_legacy_hamster_v2.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-# This file is part of 'hamster-lib'.
+# This file is part of 'nark'.
 #
-# 'hamster-lib' is free software: you can redistribute it and/or modify
+# 'nark' is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# 'hamster-lib' is distributed in the hope that it will be useful,
+# 'nark' is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with 'hamster-lib'.  If not, see <http://www.gnu.org/licenses/>.
+# along with 'nark'.  If not, see <http://www.gnu.org/licenses/>.
 
 upgrade_legacy_db() {
   local db_path
