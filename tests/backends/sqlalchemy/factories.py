03473d56ea873c450ae166ad09371d4d8e1ec0b1
HotOffThe Hamster <hotoffthehamster@gmail.com>
2018-08-24 01:51:46 -0500

    Tests: Update deprecated key-value in Alchemy Factory Meta class.
    - `force_flush = True` → `sqlalchemy_session_persistence = 'flush'`

--- tests/backends/sqlalchemy/factories.py
+++ tests/backends/sqlalchemy/factories.py
@@ -46,7 +46,7 @@ class AlchemyCategoryFactory(factory.alchemy.SQLAlchemyModelFactory):
     class Meta:
         model = AlchemyCategory
         sqlalchemy_session = common.Session
-        force_flush = True
+        sqlalchemy_session_persistence = 'flush'
 
 
 class AlchemyActivityFactory(factory.alchemy.SQLAlchemyModelFactory):
@@ -60,7 +60,7 @@ class AlchemyActivityFactory(factory.alchemy.SQLAlchemyModelFactory):
     class Meta:
         model = AlchemyActivity
         sqlalchemy_session = common.Session
-        force_flush = True
+        sqlalchemy_session_persistence = 'flush'
 
 
 class AlchemyTagFactory(factory.alchemy.SQLAlchemyModelFactory):
@@ -76,7 +76,7 @@ class AlchemyTagFactory(factory.alchemy.SQLAlchemyModelFactory):
     class Meta:
         model = AlchemyTag
         sqlalchemy_session = common.Session
-        force_flush = True
+        sqlalchemy_session_persistence = 'flush'
 
 
 class AlchemyFactFactory(factory.alchemy.SQLAlchemyModelFactory):
@@ -91,7 +91,7 @@ class AlchemyFactFactory(factory.alchemy.SQLAlchemyModelFactory):
     class Meta:
         model = AlchemyFact
         sqlalchemy_session = common.Session
-        force_flush = True
+        sqlalchemy_session_persistence = 'flush'
 
     @factory.post_generation
     def tags(self, create, extracted, **kwargs):
