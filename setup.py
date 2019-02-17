4b5b749d60e05db9ff95009731e4024b21b525a5
HotOffThe Hamster <hotoffthehamster@gmail.com>
2019-02-16 22:55:16 -0600

    Docs: Packaging: Add copyright header to setup.py.

--- setup.py
+++ setup.py
@@ -1,10 +1,31 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+# This file is part of 'nark'.
+#
+# 'nark' is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# 'nark' is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with 'nark'.  If not, see <http://www.gnu.org/licenses/>.
+
 """
 Packaging instruction for setup tools.
 
+Refs:
+
   https://setuptools.readthedocs.io/
+
+  https://packaging.python.org/en/latest/distributing.html
+
+  https://github.com/pypa/sampleproject
 """
 
 import os
