64b3f981e032e2a311a3fa6baa5cb9b5ff2dbc9a
HotOffThe Hamster <hotoffthehamster@gmail.com>
2019-02-22 16:07:50 -0600

    Packaging: Specify minimum package version, for sqlalchemy-migrate-dob.

--- setup.py
+++ setup.py
@@ -84,7 +84,7 @@ requirements = [
     #  https://pypi.org/project/sqlalchemy-migrate/
     #  https://sqlalchemy-migrate.readthedocs.io/en/latest/
     # 2019-02-21: (lb): Forked again! Package alt. that accepts static config.
-    'sqlalchemy-migrate-dob',
+    'sqlalchemy-migrate-dob >= 0.12.1',
     # https://github.com/regebro/tzlocal
     'tzlocal',
 ]
