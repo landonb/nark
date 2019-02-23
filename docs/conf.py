65f64894f45c6ee3740ca516dba611d24ef7445c
HotOffThe Hamster <hotoffthehamster@gmail.com>
2019-02-22 18:09:23 -0600

    Docs: Remove unused option value from docs/conf.py.

--- docs/conf.py
+++ docs/conf.py
@@ -190,7 +190,6 @@ html_context = {
 # A shorter title for the navigation bar.  Default is the same as
 # html_title.
 #html_short_title = None
-html_short_title = 'Nark'
 
 # The name of an image file (relative to this directory) to place at the
 # top of the sidebar.
