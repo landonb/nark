e564c47b37ef2436d5d1d1ac7f6e9231a404f2b9
HotOffThe Hamster <hotoffthehamster@gmail.com>
2020-06-20 22:41:32 -0500

    Tests: Complete coverage of items.fact.Fact class.

--- tests/items/test_fact.py
+++ tests/items/test_fact.py
@@ -19,6 +19,7 @@
 
 import copy
 import datetime
+from math import inf
 from operator import attrgetter
 
 import faker as faker_
@@ -34,6 +35,13 @@ from .test_activity import TestActivity
 from .test_tag import TestTag
 
 faker = faker_.Faker()
+
+
+# A class to test as_kvals.
+class FactWithFact(Fact):
+    def __init__(self, *args, **kwargs):
+        super(FactWithFact, self).__init__(*args, **kwargs)
+        self.mirror = self
 
 
 class TestFact(object):
@@ -233,77 +241,6 @@ class TestFact(object):
         fact.description = description_valid_parametrized
         assert fact.description == description_valid_parametrized
 
-    def test_delta(self, fact):
-        """Make sure that valid arguments get stored by the setter."""
-        assert fact.delta() == fact.end - fact.start
-
-    @freeze_time('2015-05-02 18:07')
-    def test_delta_no_end(self, fact):
-        """Make sure that a missing end datetime results in ``delta=None``."""
-        # See FactFactory for default start/end.
-        fact.end = None
-        # NOTE: With freezegun, both now() and utcnow() are the same.
-        assert fact.delta() == (datetime.datetime.now() - fact.start)
-
-    @pytest.mark.parametrize(
-        'offset',
-        [
-            (
-                15,
-                {
-                    '%M': '15',
-                    '%H:%M': '00:15',
-                    'HHhMMm': ' 0 hours 15 minutes',
-                    '': '15.00 mins.',
-                },
-            ),
-            (
-                452,
-                {
-                    '%M': '452',
-                    '%H:%M': '07:32',
-                    'HHhMMm': ' 7 hours 32 minutes',
-                    '': '7.53 hours',
-                },
-            ),
-            (
-                912,
-                {
-                    '%M': '912',
-                    '%H:%M': '15:12',
-                    'HHhMMm': '15 hours 12 minutes',
-                    '': '15.20 hours',
-                },
-            ),
-            (
-                61,
-                {
-                    '%M': '61',
-                    '%H:%M': '01:01',
-                    'HHhMMm': ' 1 hour   1 minute ',
-                    '': '1.02 hours',
-                },
-            ),
-        ],
-    )
-    def test_format_delta_valid_style(
-        self,
-        fact,
-        offset,
-        start_end_datetimes_from_offset_now,
-        string_delta_style_parametrized,
-    ):
-        """Make sure that the resulting string matches our expectation."""
-        end_offset, expectation = offset
-        fact.start, fact.end = start_end_datetimes_from_offset_now(end_offset)
-        result = fact.format_delta(style=string_delta_style_parametrized)
-        assert result == expectation[string_delta_style_parametrized]
-
-    def test_format_delta_invalid_style(self, fact):
-        """Ensure that passing an invalid format will raise an exception."""
-        with pytest.raises(ValueError):
-            fact.format_delta(style='foobar')
-
     def test_category_property(self, fact):
         """Make sure the property returns this facts category."""
         assert fact.category == fact.activity.category
@@ -432,6 +369,64 @@ class TestFact(object):
             setattr(fact, attribute, value)
         assert fact.get_serialized_string() == expectation
 
+    def test__eq__false(self, fact):
+        """Make sure that two distinct facts return ``False``."""
+        other = copy.deepcopy(fact)
+        other.pk = 1
+        assert fact is not other
+        assert fact != other
+
+    def test__eq__true(self, fact):
+        """Make sure that two identical facts return ``True``."""
+        other = copy.deepcopy(fact)
+        assert fact is not other
+        assert fact == other
+
+    def test_is_hashable(self, fact):
+        """Test that ``Fact`` instances are hashable."""
+        assert hash(fact)
+
+    def test_hash_method(self, fact):
+        """Test that ``__hash__`` returns the hash expected."""
+        assert hash(fact) == hash(fact.as_tuple())
+
+    def test_hash_different_between_instances(self, fact_factory):
+        """
+        Test that different instances have different hashes.
+
+        This is actually unneeded as we are merely testing the builtin ``hash``
+        function and ``Fact.as_tuple`` but for reassurance we test it anyway.
+        """
+        assert hash(fact_factory()) != hash(fact_factory())
+
+    def test__gt__(self, fact):
+        # A little self-referential, but covers it!
+        assert not (fact > fact)
+
+    def test__lt__(self, fact):
+        # A little self-referential, but covers it!
+        assert not (fact < fact)
+
+    def test_sorty_times(self, fact):
+        assert fact.sorty_times == (fact.start, fact.end)
+
+    def test_sorty_tuple_inf(self, fact):
+        assert fact.pk is None  # Which sorty_tuple replaces with -inf.
+        assert fact.sorty_tuple == (fact.start, fact.end, -inf)
+
+    def test_sorty_tuple_pk(self, fact):
+        fact.pk = 123
+        assert fact.sorty_tuple == (fact.start, fact.end, fact.pk)
+
+    def test_as_kvals_avoid_repr_recursion(self):
+        fact = FactWithFact(activity=None, start=None)
+        repred = fact.as_kvals()
+        assert repred.startswith('Fact(')
+
+    def test_copy_include_pk(self, fact):
+        new_fact = fact.copy(include_pk=True)
+        assert new_fact == fact
+
     def test_as_tuple_include_pk(self, fact):
         """Make sure that conversion to a tuple matches our expectations."""
         assert fact.as_tuple() == (
@@ -471,35 +466,336 @@ class TestFact(object):
         other.description += 'foobar'
         assert fact.equal_fields(other) is False
 
-    def test__eq__false(self, fact):
-        """Make sure that two distinct facts return ``False``."""
-        other = copy.deepcopy(fact)
-        other.pk = 1
-        assert fact is not other
-        assert fact != other
+    # ***
 
-    def test__eq__true(self, fact):
-        """Make sure that two identical facts return ``True``."""
-        other = copy.deepcopy(fact)
-        assert fact is not other
-        assert fact == other
+    def test_start_fmt(self, fact):
+        assert fact.start_fmt() == fact.start.strftime("%Y-%m-%d %H:%M:%S")
 
-    def test_is_hashable(self, fact):
-        """Test that ``Fact`` instances are hashable."""
-        assert hash(fact)
+    def test_start_fmt_utc_empty(self):
+        fact = Fact(activity=None, start=None)
+        assert fact.start_fmt_utc == ''
 
-    def test_hash_method(self, fact):
-        """Test that ``__hash__`` returns the hash expected."""
-        assert hash(fact) == hash(fact.as_tuple())
+    def test_start_fmt_utc_valid(self, fact):
+        formatted = fact.start_fmt_utc
+        assert formatted == fact.start.strftime("%Y-%m-%d %H:%M:%S%z")
 
-    def test_hash_different_between_instances(self, fact_factory):
-        """
-        Test that different instances have different hashes.
+    def test_start_fmt_local_empty(self):
+        fact = Fact(activity=None, start=None)
+        assert fact.start_fmt_local == ''
 
-        This is actually unneeded as we are merely testing the builtin ``hash``
-        function and ``Fact.as_tuple`` but for reassurance we test it anyway.
-        """
-        assert hash(fact_factory()) != hash(fact_factory())
+    def test_start_fmt_local_valid(self, fact):
+        formatted = fact.start_fmt_local
+        assert formatted == fact.start.strftime("%Y-%m-%d %H:%M:%S%z")
+
+    # ***
+
+    def test_end_fmt(self, fact):
+        assert fact.end_fmt() == fact.end.strftime("%Y-%m-%d %H:%M:%S")
+
+    def test_end_fmt_utc_empty(self):
+        fact = Fact(activity=None, start=None, end=None)
+        assert fact.end_fmt_utc == ''
+
+    def test_end_fmt_utc_valid(self, fact):
+        formatted = fact.end_fmt_utc
+        assert formatted == fact.end.strftime("%Y-%m-%d %H:%M:%S%z")
+
+    def test_end_fmt_local_empty(self):
+        fact = Fact(activity=None, start=None, end=None)
+        assert fact.end_fmt_local == ''
+
+    def test_end_fmt_local_valid(self, fact):
+        formatted = fact.end_fmt_local
+        assert formatted == fact.end.strftime("%Y-%m-%d %H:%M:%S%z")
+
+    @freeze_time('2015-05-02 18:07')
+    def test_end_fmt_local_nowwed_now(self):
+        fact = Fact(activity=None, start=None, end=None)
+        formatted = fact.end_fmt_local_nowwed
+        assert formatted == '2015-05-02 18:07:00 <now>'
+
+    @freeze_time('2015-05-02 18:07')
+    def test_end_fmt_local_nowwed_end(self, fact):
+        formatted = fact.end_fmt_local_nowwed
+        assert formatted == fact.end.strftime("%Y-%m-%d %H:%M:%S")
+
+#    @freeze_time('2015-05-02 18:07')
+#    def test_end_fmt_local_nowwed_now(self):
+#        fact = Fact(activity=None, start=None, end=None)
+#        formatted = fact.end_fmt_local_nowwed
+#        assert formatted == '2015-05-02 18:07:00 <now>'
+
+    @freeze_time('2015-05-02 18:07')
+    def test_end_fmt_local_or_now_end(self, fact):
+        formatted = fact.end_fmt_local_or_now
+        assert formatted == fact.end.strftime("%Y-%m-%d %H:%M:%S")
+
+    @freeze_time('2015-05-02 18:07')
+    def test_end_fmt_local_or_now_now(self):
+        fact = Fact(activity=None, start=None, end=None)
+        formatted = fact.end_fmt_local_or_now
+        assert formatted == '2015-05-02 18:07:00'
+
+    # ***
+
+    def test_momentaneous_False(self, fact):
+        assert not fact.momentaneous
+
+    def test_momentaneous_True(self, fact):
+        fact.end = fact.start
+        assert fact.momentaneous
+
+    @freeze_time('2015-05-02 18:07')
+    def test_time_now(self, fact):
+        # (lb): What's the point of accessing 'now' through the Fact? I forget.
+        assert fact.time_now == datetime.datetime.now()
+
+    def test_times(self, fact):
+        # Note that Fact.times is similar to Fact.sorty_times, except latter
+        # replaces Fact.end with UntilTimeStops if fact.end is None.
+        assert fact.times == (fact.start, fact.end)
+
+    def test_times_ok_True(self, fact):
+        assert fact.times_ok is True
+
+    def test_times_ok_False(self):
+        fact = Fact(activity=None, start=None)
+        assert fact.times_ok is False
+
+    # ***
+
+    def test_delta(self, fact):
+        """Make sure that valid arguments get stored by the setter."""
+        assert fact.delta() == fact.end - fact.start
+
+    @freeze_time('2015-05-02 18:07')
+    def test_delta_no_end(self, fact):
+        """Make sure that a missing end datetime results in ``delta=None``."""
+        # See FactFactory for default start/end.
+        fact.end = None
+        # NOTE: With freezegun, both now() and utcnow() are the same.
+        assert fact.delta() == (datetime.datetime.now() - fact.start)
+
+    @pytest.mark.parametrize(
+        'offset',
+        [
+            (
+                15,
+                {
+                    '%M': '15',
+                    '%H:%M': '00:15',
+                    'HHhMMm': ' 0 hours 15 minutes',
+                    '': '15.00 mins.',
+                },
+            ),
+            (
+                452,
+                {
+                    '%M': '452',
+                    '%H:%M': '07:32',
+                    'HHhMMm': ' 7 hours 32 minutes',
+                    '': '7.53 hours',
+                },
+            ),
+            (
+                912,
+                {
+                    '%M': '912',
+                    '%H:%M': '15:12',
+                    'HHhMMm': '15 hours 12 minutes',
+                    '': '15.20 hours',
+                },
+            ),
+            (
+                61,
+                {
+                    '%M': '61',
+                    '%H:%M': '01:01',
+                    'HHhMMm': ' 1 hour   1 minute ',
+                    '': '1.02 hours',
+                },
+            ),
+        ],
+    )
+    def test_format_delta_valid_style(
+        self,
+        fact,
+        offset,
+        start_end_datetimes_from_offset_now,
+        string_delta_style_parametrized,
+    ):
+        """Make sure that the resulting string matches our expectation."""
+        end_offset, expectation = offset
+        fact.start, fact.end = start_end_datetimes_from_offset_now(end_offset)
+        result = fact.format_delta(style=string_delta_style_parametrized)
+        assert result == expectation[string_delta_style_parametrized]
+
+    def test_format_delta_invalid_style(self, fact):
+        """Ensure that passing an invalid format will raise an exception."""
+        with pytest.raises(ValueError):
+            fact.format_delta(style='foobar')
+
+    # ***
+
+    def test_midpoint_invalid(self):
+        fact = Fact(activity=None, start=None)
+        assert fact.midpoint is None
+
+    def test_midpoint_valid(self, fact):
+        assert fact.midpoint == fact.end - ((fact.end - fact.start) / 2)
+
+    def test_time_of_day_midpoint_invalid(self):
+        fact = Fact(activity=None, start=None)
+        assert fact.time_of_day_midpoint == ''
+
+    def test_time_of_day_midpoint_valid(self, fact):
+        todm = fact.time_of_day_midpoint
+        assert ' ◐ ' in todm
+
+    def test_time_of_day_humanize_invalid_no_start_no_end(self):
+        start = datetime.datetime(2015, 12, 10, 12, 30, 33)
+        fact = Fact(activity=None, start=start, end=None)
+        assert fact.time_of_day_humanize() == ''
+
+    @freeze_time('2015-12-10 18:07')
+    def test_time_of_day_humanize_invalid_no_start_no_end_show_now(self):
+        start = datetime.datetime(2015, 12, 10, 12, 30, 33)
+        fact = Fact(activity=None, start=start, end=None)
+        todh = fact.time_of_day_humanize(show_now=True)
+        assert todh == 'Thu 10 Dec 2015 ◐ 12:30 PM — 06:07 PM'
+
+    def test_time_of_day_humanize_momentaneous(self):
+        a_moment = datetime.datetime(1974, 5, 22, 8, 24, 18)
+        fact = Fact(activity=None, start=a_moment, end=a_moment)
+        todh = fact.time_of_day_humanize()
+        assert todh == 'Wed 22 May 1974 ◐ 08:24 AM'
+
+    def test_time_of_day_humanize_same_day(self):
+        start = datetime.datetime(2015, 12, 10, 12, 30, 33)
+        end = datetime.datetime(2015, 12, 10, 13, 30, 33)
+        fact = Fact(activity=None, start=start, end=end)
+        todh = fact.time_of_day_humanize()
+        assert todh == 'Thu 10 Dec 2015 ◐ 12:30 PM — 01:30 PM'
+
+    def test_time_of_day_humanize_separate_days(self):
+        start = datetime.datetime(2015, 12, 10, 12, 30, 33)
+        end = datetime.datetime(2015, 12, 12, 13, 30, 33)
+        fact = Fact(activity=None, start=start, end=end)
+        todh = fact.time_of_day_humanize()
+        assert todh == 'Thu 10 Dec 2015 ◐ 12:30 PM — 01:30 PM Sat 12 Dec 2015'
+
+    # ***
+
+    def test_fact_activity_name_invalid(self, fact):
+        fact.activity = None
+        assert fact.activity_name == ''
+
+    def test_fact_activity_name_valid(self, fact):
+        assert fact.activity_name == fact.activity.name
+
+    def test_fact_category(self, fact):
+        assert fact.category is fact.activity.category
+
+    def test_fact_category_name_invalid(self, fact):
+        fact.activity = None
+        assert fact.category_name == ''
+
+    def test_fact_category_name_valid(self, fact):
+        assert fact.category_name == fact.activity.category.name
+
+    # ***
+
+    # (lb): Skipping fact.description* and fact.tag* methods that start
+    #       at and follow fact.description() (in the module, as of 2020-06-20)
+    #       because I see coverage provided incidentally (from other tests).
+    #       (And my goal at this moment is 100% coverage, but not necessarily
+    #       from this specific test class.)
+
+    # ***
+
+    def test_oid_actegory_empty_default(self):
+        fact = Fact(activity=None, start=None)
+        assert fact.oid_actegory() == '@'
+
+    def test_oid_actegory_empty_supplied(self):
+        fact = Fact(activity=None, start=None)
+        empty_actegory_placeholder = 'foo'
+        txt = fact.oid_actegory(empty_actegory_placeholder=empty_actegory_placeholder)
+        assert txt == empty_actegory_placeholder
+
+    def test_oid_actegory_nameless_activity(self, fact):
+        fact.activity.name = ''
+        assert fact.oid_actegory() == '@{}'.format(fact.category_name)
+
+    def test_oid_actegory_both_named_unshelled(self, fact):
+        expected = '{}@{}'.format(fact.activity_name, fact.category_name)
+        assert fact.oid_actegory() == expected
+
+    def test_oid_actegory_both_named_shellify(self, fact):
+        expected = '"{}@{}"'.format(fact.activity_name, fact.category_name)
+        assert fact.oid_actegory(shellify=True) == expected
+
+    # ***
+
+    def test_oid_description_cut_width(self, fact):
+        fact.description = 'abc de fghijkl'
+        txt = fact.oid_description(cut_width=9, sep=', ')
+        assert txt == ', abc de...'
+
+    # ***
+
+    def test_oid_tags_quote_tokens(self, fact):
+        txt = fact.oid_tags(quote_tokens=True)
+        assert txt.startswith('"#')
+
+    # ***
+
+    def test_friendly_str_basic(self, fact):
+        txt = fact.friendly_str()
+        assert txt
+
+    def test_friendly_str_localize_True(self, fact):
+        Fact.localize(localize=True)
+        txt = fact.friendly_str()
+        Fact.localize(localize=False)
+        # (lb): Being lazy. This test provides coverage, does not check
+        # that the start and end time are localized.
+        assert txt
+
+    def test_friendly_str_include_id(self, fact):
+        txt = fact.friendly_str(include_id=True)
+        assert fact.pk is None
+        assert txt.startswith('(🏭 None) ')
+
+    def test_friendly_str_cut_width(self):
+        start = datetime.datetime(2015, 12, 10, 12, 30, 33)
+        fact = Fact(activity=None, start=start, end=None)
+        txt = fact.friendly_str(cut_width_complete=16)
+        assert txt == 'at 2015-12-10...'
+
+    def test_friendly_str_show_elapsed(self, fact):
+        txt = fact.friendly_str(show_elapsed=True)
+        # (lb): Being lazy. This test provides coverage, does not check
+        # for the elapsed time, e.g., ' [3.00 hours] ' in txt.
+        assert txt
+
+    # ***
+
+    def test_fact_short(self, fact):
+        assert fact.short
+
+    # ***
+
+    def test_create_from_parsed_fact_no_activity_raises(self, fact):
+        parsed_fact = {
+            'start': fact.start,
+            'end': fact.end,
+            'activity': None,
+        }
+        with pytest.raises(ValueError):
+            Fact.create_from_parsed_fact(parsed_fact)
+
+    # ***
 
     def test__str__(self, fact):
         expect_f = '{start} to {end} {activity}@{category}: {tags}: {description}'
