12a450a372552cb4b966e7ab1f5a40c63282b76c
HotOffThe Hamster <hotoffthehamster@gmail.com>
2020-06-21 01:21:20 -0500

    Tests: Complete coverage of nark.config.__init__.py.

--- tests/config/test_config.py
+++ tests/config/test_config.py
@@ -17,12 +17,20 @@
 # You can find the GNU General Public License reprinted in the file titled 'LICENSE',
 # or visit <http://www.gnu.org/licenses/>.
 
+import datetime
 import os
 
 import pytest
 
-from nark.config import decorate_config
+from nark.config import decorate_config, ConfigRoot
 from nark.helpers.app_dirs import NarkAppDirs
+
+
+@pytest.fixture
+def config_root():
+    config_root = ConfigRoot
+    config_root.forget_config_values()
+    return config_root
 
 
 class TestNarkAppDirs(object):
@@ -141,4 +149,58 @@ class TestConfigObjToBackendConfig(object):
         configobj, expectation = configobj_instance
         result = decorate_config(configobj).as_dict(unmutated=True)
         assert result == expectation
+
+    def test_decorate_config_on_config(self, config_root):
+        """Test that decorate_config given existing config returns that config."""
+        assert decorate_config(config_root) is config_root
+
+
+class TestNarkConfigurableDb(object):
+    """Test NarkConfigurableDb ConfigRoot.section."""
+
+    def test_db_path_with_appdirs(self, tmpdir, mocker):
+        path = tmpdir.strpath
+        mocker.patch('nark.helpers.app_dirs.appdirs.user_data_dir', return_value=path)
+        # The ConfigRoot will already have been created without AppDirs setup,
+        # so tell it to forget what it knows, and it'll scan defaults again.
+        config_root = ConfigRoot
+        config_root.forget_config_values()
+        db_path = config_root['db']['path']
+        assert db_path == os.path.join(path, 'dob.sqlite')
+
+
+class TestNarkConfigurableTime(object):
+    """Test NarkConfigurableTime ConfigRoot.section."""
+
+    # *** nark.config._strptime_day_start
+
+    # These tests implicitly test nark.config._strptime_day_start.
+
+    def test_day_start_empty_string(self, config_root):
+        config_root.asobj.time.day_start.value = ''
+        assert config_root['time']['day_start'] == datetime.time(0, 0, 0)
+
+    def test_day_start_datetime_time(self, config_root):
+        time = datetime.time(23, 59, 44)
+        config_root.asobj.time.day_start.value = time
+        assert config_root['time']['day_start'] == time
+
+    def test_day_start_from_text_valid(self, config_root):
+        time = '21:34:56'
+        config_root.asobj.time.day_start.value = time
+        assert config_root['time']['day_start'] == datetime.time(21, 34, 56)
+
+    def test_day_start_from_text_invalid(self, config_root):
+        time = '25:66:67'
+        with pytest.raises(ValueError) as excinfo:
+            config_root.asobj.time.day_start.value = time
+        assert str(excinfo.value).startswith('Unrecognized value for setting')
+
+    # *** ephemeral settings
+
+    def test_tz_aware_default(self, config_root):
+        assert config_root['time']['tz_aware'] is False
+
+    def test_default_tzinfo_default(self, config_root):
+        assert config_root['time']['default_tzinfo'] == ''
 
